3772374be671b8f88e68dabb8255a2624d59116e
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-07-26 16:06:23 +0300

    working on admin order card

--- src/components/admin/order/AdminOrderCard.tsx
+++ src/components/admin/order/AdminOrderCard.tsx
@@ -1,125 +1,109 @@
-import { Link } from "react-router-dom";
-import { Col, Row } from "react-bootstrap";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { Collapse, TableCell, TableRow } from "@mui/material";
+import { Button, Col, Row } from "react-bootstrap";
+import {
+  BanknotesIcon,
+  CheckCircleIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  Cog6ToothIcon,
+  CreditCardIcon,
+} from "@heroicons/react/24/outline";
+
+import watch1 from "../../../assets/images/watch/1.jpg";
 
 const AdminOrderCard = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Link to={`/admin/orders/1`}>
-      <Row className="bg-white text-black mb-3 p-3 border-danger border">
-        <Row>
-          <Col xs={4}>
-            <h6>5121#</h6>
-            <h5></h5>
-          </Col>
-        </Row>
-      </Row>
-    </Link>
-
-    // <Link to={`/admin/orders/1`}>
-    //   <Row className="bg-white mb-4 py-4 px-1 px-lg-4 border border-1 rounded-3">
-    //     <Row className="d-flex justify-content-between px-0 mx-0">
-    //       <h6 className="fw-bold w-auto">
-    //         طلب رقم -<span className="me-1">1001</span>
-    //       </h6>
-    //       <h6 className="fw-bold w-auto">بتاريخ - (24-7-2023)</h6>
-    //     </Row>
-
-    //     <Row className="d-flex flex-column px-0 py-3 mx-0">
-    //       <h6 className="fw-bold w-auto text-secondary">
-    //         اسم العميل
-    //         <span className="text-black me-1">Ahmed ElKadah</span>
-    //       </h6>
-    //       <h6 className="fw-bold w-auto text-secondary">
-    //         عنوان الشحن{" "}
-    //         <span className="text-black me-1">
-    //           المنصورة - مدينة السلام شارع الجامع
-    //         </span>
-    //       </h6>
-    //     </Row>
-
-    //     <Row className="d-flex justify-content-between px-0 mx-0">
-    //       <div className="d-flex align-items-center w-auto">
-    //         <h6 className="w-auto text-secondary me-0 ms-1">الحالة:</h6>
-    //         <h6 className="w-auto fw-bold d-flex align-items-center">
-    //           وصل
-    //           {/* {order?.Delivered ? (
-    //             <>
-    //               <span>وصل</span>
-    //               <FontAwesomeIcon
-    //                 icon={faCircleCheck}
-    //                 className="text-success"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           ) : (
-    //             <>
-    //               <span>لم يصل</span>
-    //               <FontAwesomeIcon
-    //                 icon={faCircleXmark}
-    //                 className="text-danger"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           )} */}
-    //         </h6>
-    //       </div>
-    //       <div className="d-flex w-auto">
-    //         <h6 className="w-auto text-secondary me-0 ms-1">الدفع:</h6>
-    //         <h6 className="w-auto fw-bold">
-    //           تم الدفع
-    //           {/* {order?.isPaid ? (
-    //             <>
-    //               <span>تم</span>
-    //               <FontAwesomeIcon
-    //                 icon={faCircleCheck}
-    //                 className="text-success"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           ) : (
-    //             <>
-    //               <span>لم يتم</span>
-    //               <FontAwesomeIcon
-    //                 icon={faCircleXmark}
-    //                 className="text-danger"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           )} */}
-    //         </h6>
-    //       </div>
-    //       <div className="d-flex w-auto">
-    //         <h6 className="w-auto text-secondary me-0 ms-1">وسيلة الدفع:</h6>
-    //         <h6 className="w-auto fw-bold">
-    //           كاش
-    //           {/* {order?.paymentMethodType === "cash" ? (
-    //             <>
-    //               <span>كاش</span>
-    //               <FontAwesomeIcon
-    //                 icon={faMoneyBills}
-    //                 className="text-success"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           ) : (
-    //             <>
-    //               <span>اي حاجة</span>
-    //               <FontAwesomeIcon
-    //                 icon={faMoneyBills}
-    //                 className="text-success"
-    //                 fixedWidth
-    //               />
-    //             </>
-    //           )} */}
-    //         </h6>
-    //       </div>
-    //       <div className="d-flex align-items-end w-auto">
-    //         <h5 className="fw-bold w-auto me-0 ms-1">3000.00</h5>
-    //         <h6 className="w-auto text-secondary">جنيه</h6>
-    //       </div>
-    //     </Row>
-    //   </Row>
-    // </Link>
+    <>
+      <TableRow>
+        {/* <TableRow sx={{ "& > *": { borderBottom: "unset" } }}> */}
+        <TableCell className="mainFont border-0 text-end">1000919#</TableCell>
+        <TableCell className="mainFont border-0 text-end">
+          محمد أبوتريكة
+        </TableCell>
+        <TableCell className="mainFont border-0 text-end ">
+          <span className="truncate-two">
+            مصر - الدقهلية - المنصورة - مدينة السلام - عمارة 8 - بلوك 9
+          </span>
+        </TableCell>
+        <TableCell className="mainFont border-0 text-end">25/7/2023</TableCell>
+        <TableCell className="mainFont border-0 text-end text-nowrap">
+          25999.00 ج.م
+        </TableCell>
+        <TableCell className="mainFont border-0 text-end text-nowrap">
+          <span className="d-flex align-items-center text-danger ">
+            <CheckCircleIcon width={"18px"} className="pe-0 ps-1" />
+            لم يصل
+          </span>
+          {/* <span className="d-flex align-items-center text-success">
+      <CheckCircleIcon width={"18px"} className="pe-0 ps-1" />
+      تم التوصيل
+    </span> */}
+        </TableCell>
+        <TableCell className="mainFont border-0 text-end text-nowrap">
+          <span className="d-flex align-items-center">
+            <BanknotesIcon width={"18px"} className="pe-0 ps-1" />
+            كاش
+          </span>
+          {/* <span className="d-flex align-items-center">
+      <CreditCardIcon width={"18px"} className="pe-0 ps-1" />
+      بطاقة إلكترونية
+    </span> */}
+        </TableCell>
+        <TableCell className="mainFont border-0 text-end">
+          <span className="d-flex gap-2">
+            <Button className="p-2 rounded-3 d-flex align-items-center">
+              <Cog6ToothIcon width={"22px"} />
+            </Button>
+            <Button
+              variant="secondary"
+              className="d-flex align-items-center p-2 rounded-3"
+              aria-label="expand row"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? (
+                <ChevronUpIcon width={"22px"} />
+              ) : (
+                <ChevronDownIcon width={"22px"} />
+              )}
+            </Button>
+          </span>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell align="right" className="py-0" colSpan={8}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Row>
+              <Col
+                className="d-flex justify-content-start align-items-center mainFont"
+                // key={index}
+              >
+                <Col xs={3} sm={2} lg={1}>
+                  <img src={watch1} alt="" width={"100%"} />
+                </Col>
+                <Col xs={9} sm={10} lg={11}>
+                  <h6 className="fw-bold">
+                    ساعة يد جالاكسي 4 كلاسيك مقاس 46 مم أسود
+                  </h6>
+                  <div className="d-flex gap-4">
+                    <h6>
+                      الكمية:
+                      <span className="fw-bold"> 20</span>
+                    </h6>
+                    <h6>
+                      اللون:
+                      <span className="fw-bold"> احمر</span>
+                    </h6>
+                  </div>
+                </Col>
+              </Col>
+            </Row>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
   );
 };
 
--- src/components/admin/order/AdminOrdersContainer.tsx
+++ src/components/admin/order/AdminOrdersContainer.tsx
@@ -1,62 +1,131 @@
-import { Row } from "react-bootstrap";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import AdminOrderCard from "./AdminOrderCard";
-import { ChevronDownIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
 
 const AdminOrdersContainer = () => {
   return (
-    <Row className="px-1">
-      {/* <Table>
-        <Thead>
-          <Tr>
-            <Th>رقم الطلب</Th>
-            <Th>الاسم</Th>
-            <Th>العنوان</Th>
-            <Th>التاريخ</Th>
-            <Th>الإجمالي</Th>
-            <Th>الحالة</Th>
-            <Th>طريقة الدفع</Th>
-            <Th>الإجراءات</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>5811#</Td>
-            <Td>محمد أبوتريكة</Td>
-            <Td>قطر, الدور التاني - الشقة على اليمين</Td>
-            <Td>24/7/2023</Td>
-            <Td>25999.00 ج.م</Td>
-            <Td>قيد الإنتظار</Td>
-            <Td>عند الإستلام</Td>
-            <Td className="d-flex gap-2">
-              <div className="orderSettings">
-                <Cog6ToothIcon width={"20px"} />
-              </div>
-              <div className="orderSettings">
-                <ChevronDownIcon width={"20px"} />
-              </div>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>5811#</Td>
-            <Td>محمد أبوتريكة</Td>
-            <Td>قطر, الدور التاني - الشقة على اليمين</Td>
-            <Td>24/7/2023</Td>
-            <Td>25999.00 ج.م</Td>
-            <Td>قيد الإنتظار</Td>
-            <Td>عند الإستلام</Td>
-            <Td className="d-flex gap-2">
-              <div className="orderSettings">
-                <Cog6ToothIcon width={"20px"} />
-              </div>
-              <div className="orderSettings">
-                <ChevronDownIcon width={"20px"} />
-              </div>
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table> */}
-    </Row>
+    <div className="px-3">
+      <TableContainer component={Paper} className="shadow-none border-0">
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                رقم الطلب
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                الاسم
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                العنوان
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                التاريخ
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                الإجمالي
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                الحالة
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                طريقة الدفع
+              </TableCell>
+              <TableCell className="mainFont fw-bold text-end text-nowrap">
+                الإجراءات
+              </TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <AdminOrderCard />
+            <AdminOrderCard />
+            <AdminOrderCard />
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
 export default AdminOrdersContainer;
+
+// import { Button, Row, Table } from "react-bootstrap";
+// import AdminOrderCard from "./AdminOrderCard";
+// import {
+//   BanknotesIcon,
+//   CheckCircleIcon,
+//   ChevronDownIcon,
+//   CircleStackIcon,
+//   Cog6ToothIcon,
+//   CreditCardIcon,
+// } from "@heroicons/react/24/outline";
+
+// const AdminOrdersContainer = () => {
+//   return (
+//     <Row className="px-3">
+//       <Table responsive>
+//         <thead>
+//           <tr>
+//             <th className="py-3">رقم الطلب</th>
+//             <th className="py-3">الاسم</th>
+//             <th className="py-3">العنوان</th>
+//             <th className="py-3">التاريخ</th>
+//             <th className="py-3">الإجمالي</th>
+//             <th className="py-3">الحالة</th>
+//             <th className="py-3">طريقة الدفع</th>
+//             <th className="py-3">الإجراءات</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>5811#</td>
+//             <td>محمد أبوتريكة</td>
+//             <td>قطر - الدور التاني - الشقة على اليمين</td>
+//             <td>24/7/2023</td>
+//             <td>25999.00 ج.م</td>
+//             <td>
+//               <span className="d-flex align-items-center justify-content-start text-warning">
+//                 <CheckCircleIcon width={"18px"} className="me-0 ms-1" />
+//                 جار التحضير
+//               </span>
+//               {/* <span className="d-flex align-items-center justify-content-start text-success">
+//                 <CheckCircleIcon width={"18px"} className="me-0 ms-1" />
+//                 تم الدفع
+//               </span> */}
+//             </td>
+//             <td>
+//               {/* <span className="d-flex align-items-center justify-content-start">
+//                 <BanknotesIcon width={"18px"} className="me-0 ms-1" />
+//                 كاش
+//               </span> */}
+//               <span className="d-flex align-items-center justify-content-start">
+//                 <CreditCardIcon width={"18px"} className="me-0 ms-1" />
+//                 بطاقة إلكترونية
+//               </span>
+//             </td>
+//             <td className="d-flex gap-2">
+//               <Button className="p-2 rounded-3">
+//                 <Cog6ToothIcon width={"26px"} />
+//               </Button>
+//               {/* <div className="orderSettings">
+//                 <Cog6ToothIcon width={"20px"} />
+//               </div>
+//               <div className="orderSettings">
+//                 <ChevronDownIcon width={"20px"} />
+//               </div> */}
+//             </td>
+//           </tr>
+//         </tbody>
+//       </Table>
+//     </Row>
+//   );
+// };
+
+// export default AdminOrdersContainer;
