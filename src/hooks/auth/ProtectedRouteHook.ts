a7dd2f452262c3d414bce011eb42a04d458a4973
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-09-05 13:07:47 +0300

    protected route from redux state

--- src/hooks/auth/ProtectedRouteHook.ts
+++ src/hooks/auth/ProtectedRouteHook.ts
@@ -1,21 +1,43 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const ProtectedRouteHook = () => {
   const [isUser, setUser] = useState(false);
   const [isAdmin, setAdmin] = useState(false);
 
+  // const role = JSON.parse(localStorage?.getItem("userInfo"))?.role;
+
+  // useEffect(() => {
+  //   if (role) {
+  //     if (role == "user") {
+  //       setUser(true);
+  //       setAdmin(false);
+  //     } else if (role == "admin") {
+  //       setUser(false);
+  //       setAdmin(true);
+  //     }
+  //   } else {
+  //     setUser(false);
+  //     setAdmin(false);
+  //   }
+  // }, [role]);
+
+  const role = useSelector((state) => state.auth.user?.data?.role);
+
   useEffect(() => {
-    if (JSON.parse(localStorage?.getItem("userInfo"))?.role == "user") {
-      setUser(true);
-      setAdmin(false);
-    } else if (JSON.parse(localStorage?.getItem("userInfo"))?.role == "admin") {
-      setUser(false);
-      setAdmin(true);
+    if (role) {
+      if (role == "user") {
+        setUser(true);
+        setAdmin(false);
+      } else if (role == "admin") {
+        setUser(false);
+        setAdmin(true);
+      }
     } else {
       setUser(false);
       setAdmin(false);
     }
-  }, []);
+  }, [role]);
 
   return [isUser, isAdmin];
 };
