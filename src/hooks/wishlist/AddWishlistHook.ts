09d2c6dc8d7ca85c1c2ab26a25e573676aa1c42c
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-09-16 16:44:45 +0300

    add wishlist must be logged in

--- src/hooks/wishlist/AddWishlistHook.ts
+++ src/hooks/wishlist/AddWishlistHook.ts
@@ -2,12 +2,17 @@ import { useDispatch } from "react-redux";
 import { addWishlist, getUserWishlist } from "../../features/wishlistSlice";
 import { loggedUser } from "../../features/authSlice";
 import { getAllProducts } from "../../features/productSlice";
+import notify from "../../helpers/notify";
 
 const AddWishlistHook = (productId) => {
   const dispatch = useDispatch();
 
   const handleAddWishlist = async (e) => {
     e.persist();
+
+    if (!localStorage?.getItem("token"))
+      return notify("من فضلك قم بتسجيل الدخول أولاً", "warn");
+
     await dispatch(addWishlist({ productId }));
     await dispatch(loggedUser());
     dispatch(getUserWishlist());
