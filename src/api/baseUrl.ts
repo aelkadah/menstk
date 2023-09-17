45e6a3eb92805faf8a4cf2f89235475dd8ce2b12
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-09-18 00:30:01 +0300

    deleted assets unwanted images

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const baseUrl = axios.create({ baseURL: "http://127.0.0.1:8000" });
+// const baseUrl = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const baseUrl = axios.create({
+  baseURL: "https://menstk-backend.onrender.com",
+});
 
 export default baseUrl;
